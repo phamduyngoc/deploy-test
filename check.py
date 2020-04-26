e9a4379e28a7708e35be9e7b93ab3413c6964b09
phamduyngoc <pdn16091998@gmail.com>
2020-04-26 17:27:14 +0700

    update

--- check.py
+++ check.py
@@ -1,16 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 from flask import Flask
-
 app = Flask(__name__)
-
 
 @app.route("/")
 def hello():
     return "Hello PyMi.vn!"
-
-# In[ ]:
+app.run()
